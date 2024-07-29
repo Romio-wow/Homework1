eccd3f13d10a6c209bff6d33829857fa559dd7a3
Roman <roman.me@mail.ru>
2024-07-29 13:48:35 +0300

    Add Test2

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,3 @@
-import org.junit.Test;
-
 public class MainClass {
 
 //  метод, который возвращает число 14
@@ -7,6 +5,15 @@ public class MainClass {
         int number = 14;
         return number;
 
+    }
+
+    //приватное поле класса, которое равно 20
+    private int class_number = 20;
+
+
+    //публичный метод (getClassNumber), который возвращает переменную class_number
+    public int getClassNumber(){
+        return class_number;
     }
 
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -4,11 +4,21 @@ import org.junit.Test;
 public class MainClassTest extends MainClass {
 
 
-// тест, проверяющий, что метод getLocalNumber возвращает число 14
+    // тест, проверяющий, что метод getLocalNumber возвращает число 14
     @Test
     public void testGetLocalNumber() {
         int expected = 14;
         Assert.assertEquals("Ожидаемое число неверное", expected, getLocalNumber());
+
+    }
+
+    // тест , который проверяет, что метод getClassNumber возвращает число больше 45.
+    @Test
+    public void testGetClassNumber() {
+        int classNumber = getClassNumber();
+
+        Assert.assertTrue("Возвращаемое число должно быть больше 45", classNumber > 45);
+
 
     }
 }
